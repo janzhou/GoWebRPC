1628d9a69076d2b593a36b8ae1a158dd60df594d
Jian Zhou <zhoujian@tiaozhanshu.com>
2013-11-06 00:11:36 +0800

    jsonrpc.js: client

---
+++ htdocs/jsonrpc.js
@@ -0,0 +1,57 @@
+var jsonrpc = {
+    version: 0.1,
+    maxRequest: 10
+};
+
+jsonrpc.Call = function(request){
+    while( this.request[this.i] != null ) {
+        this.i++;
+        if(this.i >= this.maxRequest) {
+            this.i = 0;
+        };
+    }
+
+    this.request[this.i] = request;
+    var data = '{"method": "'+request.method+'", "params": ['+JSON.stringify(request.args)+'], "id": '+this.i+'}';
+    this.ws.send(data);
+};
+
+jsonrpc.onclientmessage = function(e) {
+    var client = e.originalTarget.parrent;
+    var d = e.data;
+    ret = JSON.parse(e.data, function (key, value) {
+        var type;
+        if (value && typeof value === 'object') {
+            type = value.type;
+            if (typeof type === 'string' && typeof window[type] === 'function') {
+                return new (window[type])(value);
+            }
+        }
+        return value;
+    });
+
+    if(ret.error == null) {
+        client.request[ret.id].success(ret.result);
+    } else {
+        client.request[ret.id].error(ret.error);
+    };
+    client.request[ret.id] = null;
+};
+
+jsonrpc.Close = function(){
+    this.ws.Close;
+};
+
+jsonrpc.NewClient = function(addr){
+    var client = {};
+    client.i = 0;
+    client.maxRequest = jsonrpc.maxRequest;
+    client.request = new Array();
+    client.Addr = addr;
+    client.ws = new WebSocket(addr);
+    client.ws.parrent = client;
+    client.ws.onmessage = jsonrpc.onclientmessage;
+    client.Call = jsonrpc.Call;
+    client.Close = jsonrpc.Close;
+    return client;
+};
