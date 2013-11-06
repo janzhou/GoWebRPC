9397d774a2cc07e1f87149b5e4b2200f40167c93
Jian Zhou <zhoujian@tiaozhanshu.com>
2013-11-06 23:27:43 +0800

    seperate arith.go

---
+++ arith.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "errors"
+)
+
+type Args struct {
+    A int
+    B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+type Arith int
+
+func (t *Arith) Multiply(args *Args, reply *int) error {
+    *reply = args.A * args.B
+    return nil
+}
+
+func (t *Arith) Divide(args *Args, quo *Quotient) error {
+	if args.B == 0 {
+		return errors.New("divide by zero")
+	}
+	quo.Quo = args.A / args.B
+	quo.Rem = args.A % args.B
+	return nil
+}
--- websocket.go
+++ websocket.go
@@ -5,33 +5,7 @@ import (
     "net/rpc/jsonrpc"
     "io"
     "log"
-    "errors"
 )
-
-type Args struct {
-    A int
-    B int
-}
-
-type Quotient struct {
-	Quo, Rem int
-}
-
-type Arith int
-
-func (t *Arith) Multiply(args *Args, reply *int) error {
-    *reply = args.A * args.B
-    return nil
-}
-
-func (t *Arith) Divide(args *Args, quo *Quotient) error {
-	if args.B == 0 {
-		return errors.New("divide by zero")
-	}
-	quo.Quo = args.A / args.B
-	quo.Rem = args.A % args.B
-	return nil
-}
 
 func rpcHandler(ws *websocket.Conn) {
     jsonrpc.ServeConn(ws)
